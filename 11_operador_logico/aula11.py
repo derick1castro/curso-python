bb8568fb45db690ab6eaaab5fd2d595aef7c99eb
Derick Castro <derick1castro@gmail.com>
2022-08-04 13:18:58 -0300

    Studies about Python

--- 11_operador_logico/aula11.py
+++ 11_operador_logico/aula11.py
@@ -3,3 +3,20 @@ Operadores Lógicos
 and, or, not
 in e not in
 """
+nome = 'Derick Castro'
+
+#if 'r' in nome:
+#   print("Existe a letra R no seu nome.")
+#else:
+#    print("Não existe")
+
+usuario = input('Nome de usuário: ')
+senha = input('Senha do usuário: ')
+
+usuario_bd = 'derick'
+senha_bd = '123456'
+
+if usuario_bd == usuario and senha_bd == senha:
+    print('Você está logado no sistema.')
+else:
+    print('Usuário ou senha inválidos.')
---
+++ 14_formatar_valores/aula13.py
@@ -0,0 +1,13 @@
+"""
+Formatando valores com modificadores
+
+:s - Texto (strings)
+:d - Inteiros (int)
+:f - Números de ponto flutuante (float)
+:.(NÚMERO)f - Quantidade de casas decimais (float)
+:(CARACTERE) (> ou < ou ^)(QUANTIDADE)(tipo - s, d ou f)
+
+> - Esquerda
+< - Direita
+^ - Centro
+"""
